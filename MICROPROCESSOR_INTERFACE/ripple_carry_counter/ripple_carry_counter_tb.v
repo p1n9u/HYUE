92f00b54b50e5b475bca9f0b7d3af8afb165f607
p1n9u <cybrphntm4@hanyang.ac.kr>
2020-10-27 18:06:47 +0900

    Update ripple_carry_counter_tb.v

--- MICROPROCESSOR_INTERFACE/ripple_carry_counter/ripple_carry_counter_tb.v
+++ MICROPROCESSOR_INTERFACE/ripple_carry_counter/ripple_carry_counter_tb.v
@@ -1,21 +1,24 @@
 module ripple_carry_counter_stimulus;
-reg clk, reset; wire[3:0] q; 
-// instantiate the design block 
-ripple_carry_counter r1(q, clk, reset); 
-// Control the clk signal that drives the design block. Cycle time = 10 
-initial 
-	clk = 1'b0; //set clk to 0 
-always 
-	#5 clk = ~clk; //toggle clk every 5 time units 
-// Control the reset signal that drives the design block,
-// reset is asserted from 0 to 20 and from 200 to 220. 
-initial 
-begin 
-	reset = 1'b1; 
-	#15 reset = 1'b0; #180 reset = 1'b1; #10 reset = 1'b0; #20 $finish; 
-//terminate the simulation 
-end 
-// Monitor the outputs 
-initial 
-$monitor($time, " Output q = %d", q); 
+    reg clk, reset;
+    wire[3:0] q; 
+ 
+    ripple_carry_counter r1(q, clk, reset); 
+
+    initial 
+        clk = 1'b0;
+	
+    always 
+        #5 clk = ~clk;
+	
+    initial 
+    begin
+        reset = 1'b1;
+        #15 reset = 1'b0;
+        #180 reset = 1'b1;
+        #10 reset = 1'b0;
+        #20 $finish; 
+    end
+	
+    initial 
+    $monitor($time, " Output q = %d", q); 
 endmodule
