8d7edf83743ed600c9f80dac60884f19a61eb05f
p1n9u <cybrphntm4@hanyang.ac.kr>
2020-10-27 18:43:01 +0900

    Update level_to_pulse_converter.v

--- MICROPROCESSOR_INTERFACE/level_to_pulse_converter/level_to_pulse_converter.v
+++ MICROPROCESSOR_INTERFACE/level_to_pulse_converter/level_to_pulse_converter.v
@@ -1,34 +1,32 @@
 module LevelToPulse(level, clk, reset, pulse);
+    input level;
+    input clk;
+    input reset;
+    output pulse;
+    reg pulse;
+    reg prestate, nextstate;
 
-input level;
-input clk;
-input reset;
-output pulse;
-reg pulse;
+    parameter s0=0, s1=1;
 
-reg prestate, nextstate;
-parameter s0=0, s1=1;
+    always @(posedge clk, posedge reset)
+        if (reset)
+	    prestate <= s0;
+        else
+	    prestate <= nextstate;
 
-always @(posedge clk, posedge reset)
-if (reset)
-	prestate <= s0;
-else
-	prestate <= nextstate;
-
-always @(prestate, level)
-begin
-	nextstate = prestate;
+    always @(prestate, level)
+    begin
+        nextstate = prestate;
 	pulse = 1'b0;
 	case(prestate)
- 		s0: 
-			if(level)
- 			begin
- 				pulse = 1'b1; nextstate = s1;
- 			end
- 		s1: 
-			if(~level)
- 				nextstate = s0;
+ 	    s0: 
+	        if(level)
+ 		begin
+ 		    pulse = 1'b1; nextstate = s1;
+ 	        end
+ 	    s1: 
+		if(~level)
+ 		    nextstate = s0;
  	endcase
-end
-
+    end
 endmodule 
