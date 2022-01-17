aed943076970f6361ff6da7034fcf838fbf04443
p1n9u <cybrphntm4@hanyang.ac.kr>
2022-01-17 19:03:04 +0900

    mod

--- PROGRAMMING_METHODOLOGY/sw/code/final19/class1/answer/Q1.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Q1 {
-	public static void main(String[] args) throws IOException {
-		PrintWriter outfile = new PrintWriter(new FileWriter("test"));
-		outfile.println("Hello world!");
-		outfile.close();
-	}
-}
